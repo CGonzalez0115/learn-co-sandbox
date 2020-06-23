0a7d9ed4d28f207a8fb15c7991f9e43bacfc34b1
Christina Gonzalez <cmgamway@gmail.com>
2020-06-23 03:12:55 +0000

    Automatically backed up by Learn

--- art_through_the_years/lib/art_through_the_years.rb
+++
@@ -1,6 +0,0 @@
-require "art_through_the_years/version"
-
-module ArtThroughTheYears
-  class Error < StandardError; end
-  # Your code goes here...
-end
--- art_through_the_years/lib/art_through_the_years/version.rb
+++
@@ -1,3 +0,0 @@
-module ArtThroughTheYears
-  VERSION = "0.1.0"
-end
